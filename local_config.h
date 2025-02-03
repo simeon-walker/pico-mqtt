b73c2f43b883a6dc55bbb0ecad106f58a7ada48b
Simeon <simeon-walker@users.noreply.github.com>
2025-02-03 20:27:27 +0000

    add local config

---
+++ local_config.h
@@ -0,0 +1,12 @@
+#define MQTT_CLIENTID "avcontroller"
+#define MQTT_PREFIX "avcontrol/#"
+
+#define TOPIC_RELAY_PIN "avcontrol/relay/pin"
+#define TOPIC_RELAY_CONTROL "avcontrol/relay/control"
+#define TOPIC_RELAY_STATUS "avcontrol/status/relay"
+#define TOPIC_IP_ADDRESS "avcontrol/status/ip_address"
+#define TOPIC_UPTIME "avcontrol/status/uptime"
+#define TOPIC_IR_RX "avcontrol/ir/rx"
+
+#define RELAY_PIN 6
+#define IR_RX_PIN 28
